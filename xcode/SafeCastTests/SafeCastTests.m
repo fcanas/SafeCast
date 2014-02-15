7a4d7964cf1ec46bc75345930809311631915026
Fabian Canas <fcanas@gmail.com>
2014-02-14 20:06:17 -0500

    Add failing test stub for `+cast:intoBlock:`. I need to come back and finish that later.

--- xcode/SafeCastTests/SafeCastTests.m
+++ xcode/SafeCastTests/SafeCastTests.m
@@ -3,8 +3,27 @@
 //  SafeCastTests
 //
 //  Created by Fabian Canas on 2/13/14.
-//  Copyright (c) 2014 Fabián Cañas. All rights reserved.
 //
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2014 Fabian Canas
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import <XCTest/XCTest.h>
 #import <SafeCast/NSObject+SafeCast.h>
@@ -36,6 +55,20 @@
     XCTAssertEqual(ma, a, @"Should successfully cast a (mutable) array to a mutable array");
     XCTAssertNil([NSMutableArray cast:s], @"Should not cast a string to a mutable array");
     XCTAssertNil([NSMutableArray cast:[NSArray array]], @"Should not cast an array to a mutable array");
+}
+
+- (void)testCastIntoBlock
+{
+//    NSString *s = @"string";
+//    NSArray *a = [NSMutableArray array];
+//    NSMutableArray *ma = [NSMutableArray cast:a];
+    
+//    void(^addObjectBlock)(NSMutableArray *) = ^()
+    
+//    XCTAssertEqual(ma, a, @"Should successfully cast a (mutable) array to a mutable array");
+//    XCTAssertNil([NSMutableArray cast:s], @"Should not cast a string to a mutable array");
+//    XCTAssertNil([NSMutableArray cast:[NSArray array]], @"Should not cast an array to a mutable array");
+    XCTFail(@"I haven't written this yet. I need to go make cookies.");
 }
 
 @end
